b203d6b8a3900eb3e026559f303d587dcfd3a7c1
Jesus Galan <yiyu.jgl@gmail.com>
2015-10-14 00:29:10 +0000

    clarify what happens when using TARGET

--- plan9-install.sh
+++ plan9-install.sh
@@ -7,7 +7,8 @@ USAGE='Usage: plan9-install.sh [ TARGET ]
 Create a plan9.tcz extension from $PLAN9, if available, or from the latest
 version in github. In the former case, create also a plan9-local.tcz
 extension that loads plan9.tcz when the original $PLAN9 is not found.
-If TARGET is specified, plan9port is installed there first.'
+If TARGET is specified, install there first, later create plan9-local.tcz
+and, if installing from github, plan9.tcz'
 
 : ${TMP:=/tmp/plan9port}
 : ${URL:=https://github.com/9fans/plan9port}
